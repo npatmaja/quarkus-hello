2c4315931191b0f1f311cea56683947c095bfddc
Nauval Atmaja <nauval.atmaja@advotics.com>
2020-05-04 12:02:50 +0700

    Update run postgresql
    Because:
    When the docker with the same name have been run once, it should not
    re-run but only to be started to avoid error docker container with the
    same name exists.
    
    In the run script first is to check container with the given name, if
    exists then start the container otherwise create new container
    
    This commit:
    Update `pg.sh`

--- pg.sh
+++ pg.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 
-docker run -d --name pg-quarkus-hello \
-	-v pg-quarkus-hello:/var/lib/postgresql/data \
-	-e POSTGRES_PASSWORD=p4ssw0rd \
-	-e POSTGRES_USER=hello \
-	-p 5432:5432 \
-	postgres:12.2-alpine
+containerName=pg-quarkus-hello
+
+containerId=$(docker inspect -f '{{.Id}}' $containerName)
+
+if [ -n "$containerId" ]; then
+	docker start $containerName
+else
+	docker run -d --name $containerName \
+		-v pg-quarkus-hello:/var/lib/postgresql/data \
+		-e POSTGRES_PASSWORD=p4ssw0rd \
+		-e POSTGRES_USER=hello \
+		-p 5432:5432 \
+		postgres:12.2-alpine
+fi
+
 
